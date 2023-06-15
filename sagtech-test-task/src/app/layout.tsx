c874fc997c2bb1c356c4fc953c26a5f6fabbd73a
cptShell <repozheka@gmail.com>
2023-06-15 13:25:56 +0300

    refactor: remove useless styles

--- sagtech-test-task/src/app/layout.tsx
+++ sagtech-test-task/src/app/layout.tsx
@@ -1,9 +1,9 @@
-import { Inter } from 'next/font/google';
+// import { Inter } from 'next/font/google';
 import { AppProvider } from '@/store/provider';
 import { Header } from './components/header/header';
 import './globals.scss';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Header />
         <AppProvider>{children}</AppProvider>
       </body>
--- sagtech-test-task/src/store/provider.tsx
+++ sagtech-test-task/src/store/provider.tsx
@@ -2,9 +2,10 @@
 
 import { Provider } from 'react-redux';
 import store from './store';
+import { ReactNode } from 'react';
 
 type Props = {
-  children: string | JSX.Element | JSX.Element[];
+  children: ReactNode;
 };
 export function AppProvider({ children }: Props) {
   return <Provider store={store}>{children}</Provider>;
